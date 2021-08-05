d1335147e80af5953f33d895af2bca40e63c97ca
Cobular <22972550+JakeCover@users.noreply.github.com>
2021-08-04 22:12:28 -0700

    Updated to new scoresaber API, removed jquery, and used async XMLHttpRequest

--- main.js
+++ main.js
@@ -1,22 +1,30 @@
-function getKey(url) {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", url, false);
-    xmlHttp.send(null);
-    return xmlHttp.responseText;
+function xhrSuccess() {
+  this.callback.apply(this, this.arguments);
 }
 
-let downloadlink = "";
+function xhrError() {
+  console.error(this.statusText);
+}
 
-let song_hash = $("div.has-shadow > b").text();
-console.log(song_hash);
-song_hash = /[a-fA-F0-9]{40}/.exec(song_hash);
+function loadFile(url, callback /*, opt_arg1, opt_arg2, ... */) {
+  var xhr = new XMLHttpRequest();
+  xhr.callback = callback;
+  xhr.arguments = Array.prototype.slice.call(arguments, 2);
+  xhr.onload = xhrSuccess;
+  xhr.onerror = xhrError;
+  xhr.open("GET", url, true);
+  xhr.send(null);
+}
 
-let key = JSON.parse(getKey(`https://beatsaver.com/api/maps/by-hash/${song_hash}`)).key
 
+function showButton() {
+  let key = JSON.parse(this.responseText).id
 
+  let downloadButtonElement = document.createElement('div');
 
-$(function () {
-    $(".column .is-one-quarter").append(`
+  const imageUrl = chrome.runtime.getURL('download-icon.svg');
+
+  downloadButtonElement.innerHTML = `
         <script async defer data-website-id="3f56052c-27bd-4235-a8f4-ad333331b91c" data-host-url="https://umami.cobular.com/">
             // My custom analytics package. This is a more privacy preserving option than Google analytics, it's open source here: https://umami.is/! 
             !function(){"use strict";var t=function(t,e,n){var a=t[e];return function(){for(var e=[],i=arguments.length;i--;)e[i]=arguments[i];return n.apply(null,e),a.apply(t,e)}},e=function(){var t=window.doNotTrack,e=window.navigator,n=window.external,a=t||e.doNotTrack||e.msDoNotTrack||n&&"function"==typeof n.msTrackingProtectionEnabled&&n.msTrackingProtectionEnabled();return!0===a||1===a||"yes"===a||"1"===a};!function(n){var a=n.screen,i=a.width,r=a.height,o=n.navigator.language,c=n.location,s=c.hostname,u=c.pathname,l=c.search,d=n.localStorage,f=n.sessionStorage,v=n.document,p=n.history,m=v.querySelector("script[data-website-id]");if(m){var h,g=function(t){return m&&m.getAttribute(t)},w=g("data-website-id"),y=g("data-host-url"),S="false"!==g("data-auto-track"),E=g("data-do-not-track"),k=g("data-cache"),b=g("data-domains"),T=d.getItem("umami.disabled")||E&&e()||b&&!b.split(",").map((function(t){return t.trim()})).includes(s),N=y?(h=y)&&h.length>1&&h.endsWith("/")?h.slice(0,-1):h:m.src.split("/").slice(0,-1).join("/"),j=i+"x"+r,q=[],I=""+u+l,L=v.referrer,O=function(t,e,n){if(!T){var a="umami.cache",i={website:n,hostname:s,screen:j,language:o,cache:k&&f.getItem(a)};e&&Object.keys(e).forEach((function(t){i[t]=e[t]})),function(t,e,n){var a=new XMLHttpRequest;a.open("POST",t,!0),a.setRequestHeader("Content-Type","application/json"),a.onreadystatechange=function(){4===a.readyState&&n&&n(a.response)},a.send(JSON.stringify(e))}(N+"/api/collect",{type:t,payload:i},(function(t){return k&&f.setItem(a,t)}))}},P=function(t,e,n){return void 0===t&&(t=I),void 0===e&&(e=L),void 0===n&&(n=w),O("pageview",{url:t,referrer:e},n)},x=function(t,e,n,a){return void 0===e&&(e="custom"),void 0===n&&(n=I),void 0===a&&(a=w),O("event",{event_type:e,event_value:t,url:n},a)},A=function(){v.querySelectorAll("[class*='umami--']").forEach((function(t){t.className.split(" ").forEach((function(e){if(/^umami--([a-z]+)--([\\w]+[\\w-]*)$/.test(e)){var n=e.split("--"),a=n[1],i=n[2],r=function(){return x(i,a)};q.push([t,a,r]),t.addEventListener(a,r,!0)}}))}))},H=function(){q.forEach((function(t){var e=t[0],n=t[1],a=t[2];e&&e.removeEventListener(n,a,!0)})),q.length=0},R=function(t,e,n){if(n){H(),L=I;var a=n.toString();(I="http"===a.substring(0,4)?"/"+a.split("/").splice(3).join("/"):a)!==L&&P(I,L),setTimeout(A,300)}};if(!n.umami){var _=function(t){return x(t)};_.trackView=P,_.trackEvent=x,_.addEvents=A,_.removeEvents=H,n.umami=_}S&&!T&&(p.pushState=t(p,"pushState",R),p.replaceState=t(p,"replaceState",R),P(I,L),A())}}(window)}();
@@ -87,15 +95,20 @@ $(function () {
                 visibility: visible;
                 opacity: 1;
             }
-
             
         </style>
         <div style="width: 64px">
             <hr style="margin: 0.1rem 0 0.5rem;">
-            <a href="beatsaver://${key}" class="button tooltip saberSaverButton umami--click--download-button-${key}"><img src="https://bsaber.com/wp-content/themes/beastsaber-wp-theme/assets/svg/ico-cloud-download.svg">
+            <a href="beatsaver://${key}" class="button tooltip saberSaverButton umami--click--download-button-${key}"><img src="${imageUrl}" alt="Download Button" title="Download Button">
                   <span class="tooltiptext">${chrome.i18n.getMessage("button_text")}</span>
             </a>
         </div>
-    `);
-});
+    `;
+
+  document.querySelector(".column .is-one-quarter").appendChild(downloadButtonElement)
+}
+
+let song_hash = document.querySelector("div.has-shadow > b").textContent;
+console.log(song_hash);
+loadFile(`https://api.beatsaver.com/maps/hash/${song_hash}`, showButton)
 
